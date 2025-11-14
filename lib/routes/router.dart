c13242c6c01fe5a104d3635870124bc8dd055295
mahendrak <bimamahendra0705@gmail.com>
2025-11-14 21:24:56 +0700

    add logic login

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,16 +1,16 @@
-import 'package:cargo_app/auth/login.dart';
+import 'package:cargo_app/auth/testlogin.dart';
 import 'package:cargo_app/landing/landing.dart';
 import 'package:cargo_app/presentations/kurir/beranda/beranda-kurir.dart';
 import 'package:cargo_app/presentations/kurir/beranda/foto-kurir.dart';
 import 'package:cargo_app/presentations/pic/beranda/beranda-pic.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
-    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+    GoRoute(path: '/Login', builder: (context, state) => const LoginScreen()),
+    //GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(
       path: '/beranda_kurir',
       builder: (context, state) => const BerandaKurirScreen(),
